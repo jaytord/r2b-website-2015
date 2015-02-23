0beb35b27175f4ac3e800fca98f9bbb731148167
Jason Tordsen <jason@jasons-mbp.home>
2015-02-22 23:35:24 -0500

    moving images

--- php/application/views/pages/casestudies_view.php
+++ php/application/views/pages/casestudies_view.php
@@ -7,7 +7,7 @@
 	<div class="cfm-project-gallery">
 		<ul>
 			<?php foreach ($data as $key => $casestudy): ?>
-			<li data-id="<?= $casestudy->id; ?>" data-image="<?= base_url().'img/casestudies/'.$casestudy->image.'.jpg'; ?>">
+			<li data-id="<?= $casestudy->id; ?>" data-image="<?= base_url().'img/projects/'.$casestudy->image.'.jpg'; ?>">
 				<div class="project-inner">
 					<a class="cfm-project" href="<?= base_url(); ?>project/<?= $casestudy->id; ?>" data-navigate-to="project/<?= $casestudy->id; ?>">
 						<div class="project-label"><div class="project-label-inner"><h3><?= $casestudy->title; ?></h3></div></div>
