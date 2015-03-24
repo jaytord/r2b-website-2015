0d0dc8c757d500e8a314a9393127f13683bb5630
Jason Tordsen <jason@jasons-mbp.home>
2015-03-23 22:58:49 -0400

    new about page

--- php/application/views/pages/about_view.php
+++ php/application/views/pages/about_view.php
@@ -2,7 +2,9 @@
 	<div class="page-header"><div class="page-header-inner">
 		<h1>ABOUT US</h1>
 		<div class="page-header-description">
-			A strategically- driven creative agency specializing in digital marketing, communications and content development, we specialize in integrated marketing strategies built to evolve with the ever-changing landscape, both on and offline. Born from award-winning design studio <a href="http://www.click3x.com">Click <span style="font-family:serif;">3</span>X</a>, our team consists of a diverse group of over 100 talented individuals that embody left and right brain disciplines.
+			<p>The industry today is changing, quickly. The ability to define it as it continues to evolve is even more challenging. As advertisers become marketers, marketing blends with PR and digital dominates...well, everything, it's easy to fall into the identity crisis conundrum in an effort to sell yourself as the answer to any and every brand need.</p>
+			<p>For us, it's simple: building things is what we do best, it's how we began and what sets us apart. While most agencies today are rushing to establish their own version of a production house, we've had that covered for more than 20 years, only with a highly-specialized strategic arm to guide it.</p>
+            <p>For every big idea and successful strategy born from our intellectual incubator of sorts, we have an in-house army of creative talent able to bring those ideas to life.</p>
 		</div>	
 	</div></div>
 	<div class="section-header">
@@ -11,43 +13,58 @@
 	<div class="services">
 		<div class="services-section">
 			<h2>Digital Marketing</h2>
-			<ul>
-				<li>Marketing across the paid, owned and earned landscape, online and offline, is no easy feat. Consider us your special ops digital team, focused on marrying your marketing efforts with focused digital marketing and media strategies. Our goal? Ensuring you have a tailored, integrated presence, where and when you should be, with impactful messaging and creative built to live across platforms.</li>
-			</ul>
+			<p>Marketing across the paid, owned and earned landscape, online and offline, is no easy feat. Consider us your special ops digital team, focused on marrying your marketing efforts with focused digital marketing and media strategies. Our goal? Ensuring you have a tailored, integrated presence, where and when you should be, with impactful messaging and creative built to live across platforms.</p>
 		</div>
  		<div class="services-section">
 			<h2>Communications</h2>
-			<ul>
-				<li>Gone are the days of vertical media outlets and controlled consumer entry points to branded content.  As traditional PR, media, and marketing continue to morph into the melting pot brands are now tasked with navigating, one indispensable purpose remains: the ability to establish meaningful engagement through storytelling. Whether developing a comprehensive social marketing campaign or informing a high-level PR strategy, we’re well-accustomed to working with agency partners and across internal teams to integrate communications efforts and ensure for consistent messaging across all media touch points.</li>
-			</ul>
+			<p>Gone are the days of vertical media outlets and controlled consumer entry points to branded content. As traditional PR, media, and marketing continue to morph into the melting pot brands are now tasked with navigating, one indispensable purpose remains: the ability to establish meaningful engagement through storytelling. Whether developing a comprehensive social marketing campaign or informing a high-level PR strategy, we’re well-accustomed to working with agency partners and across internal teams to integrate communications efforts and ensure for consistent messaging across all media touch points.</p>
 		</div>
 	 	<div class="services-section">
-			<h2>Content Development</h2>
-			<ul>
-				<li>Positioned at the intersection of entertainment, advertising, and technology, and working with our parent company <a href="http://www.click3x.com">Click <span style="font-family:serif;">3</span>X</a>, a content powerhouse, our expert team of Designers, Animators, Technologists, Strategists, and Producers create innovative, solutions-driven digital experiences that shape the media landscape. Our expertise covers the full content ecosystem, from concept, to in-house production and editing, and content marketing and distribution, making content development seamless from start to finish.</li>
-			</ul>
+			<h2>Content Creation: Design + Build</h2>
+			<p>Positioned at the intersection of entertainment, advertising, and technology and working with our parent company <a target="_blank" href="http://www.click3x.com">Click <span style="font-family:serif;">3</span>X</a>, our expert team of designers, directors, animators, producers and technologists create innovative, solutions-driven digital experiences that shape the media landscape.</p>
+			<p>Our video expertise covers the full content ecosystem, from concept, to in-house design, animation, production and editing though to content marketing and distribution, making content development seamless from start to finish.</p>
+			<p>Our robust, cost-effective digital execution capabilities include websites, display ads, social media content, mobile apps and out-of-home interactive experiences.</p>
 		</div>
 	</div>
 	<div class="section-header">
 		<h2>Executive Team</h2>
 	</div>
-	<div class="cfm-project-gallery">
-		<ul>
-			<li id="peter" data-image="<?= base_url(); ?>img/peter.jpg">
-				<div class="project-inner">
-					<a class="cfm-project"></a><div class="project-label"><div class="project-label-inner"><h2>Peter Corbett</h2><h3>President</h3></div></div>
+	<div class="services">
+		<div class="services-section clearfix">
+			<img align="left" src="<?= base_url(); ?>img/peter.jpg" />
+			<div class="project-label">
+				<div class="project-label-inner">
+					<h2>Peter Corbett</h2>
+					<h3>President</h3>
+					<p>Peter Corbett is the founder and President of <a target="_blank" href="http://www.click3x.com">Click <span style="font-family:serif;">3</span>X</a>, the parent company to Raison D’Être. This group of companies now employs over 100 professionals, which are based in New York City.</p>
+					<p>Previously Peter enjoyed a remarkable career as an advertising agency creative and commercial director. In 1995, Peter founded Media Circus, one of the first interactive agencies. In 2001 <a target="_blank" href="http://www.click3x.com">Click <span style="font-family:serif;">3</span>X</a> and its digital shop <a target="_blank" href="http://www.clickfiremedia.com">ClickFire Media</a> evolved into a content creation powerhouse experiences, working across the media landscape for agencies, TV networks and brands.</p>
+					<p>Peter’s awards include several Cannes Lions, Clios and The Crystal Apple Award from the City of New York for his contributions to the film industry.</p>
 				</div>
-			</li>
-			<li id="jessica" data-image="<?= base_url(); ?>img/jessica.jpg">
-				<div class="project-inner">
-					<a class="cfm-project"></a><div class="project-label"><div class="project-label-inner"><h2>Jessica Adams de Guardiola</h2><h3>Partner, Strategic Communications</h3></div></div>
+			</div>
+		</div>
+		<div class="services-section clearfix">
+			<img align="left" src="<?= base_url(); ?>img/jessica.jpg" />
+			<div class="project-label">
+				<div class="project-label-inner">
+					<h2>Jessica Adams de Guardiola</h2>
+					<h3>Partner, Strategic Communications</h3>
+					<p>Jessica is an award-winning public relations executive, with extensive experience in media relations, campaign development and strategic branding. Prior to joining Raison D'Etre, Jessica held executive positions at Regan Communications and most recently, HL Group. During her time at HL Group, Jessica oversaw campaign strategies fornational brands including Amazon Fashion, Converse, DVF's 40th Anniversary Journey of a Dress, Hudson Jeans and Oscar de la Renta Kids to name a few.</p>
+					<p>In 2008, Jessica founded 5s Public Relations, a Boston-based boutique PR firm specializing in fashion, beauty and lifestyle clientele and Studio 5 Showroom, Boston’s first fashion showroom and recipient of the Boston Globe’s Best of 2011 Award. In 2011, 5sPR was acquired by Regan Communications Group, where Jessica was brought on asvice president. She managed media relations strategies for a wide range of hospitality and lifestyle clients including renowned venues Capitale and espace, Mohegan Sun and the David Ortiz Celebrity Golf Classic.</p>
 				</div>
-			</li>
-			<li id="heather" data-image="<?= base_url(); ?>img/heather.jpg">
-				<div class="project-inner">
-					<a class="cfm-project"></a><div class="project-label"><div class="project-label-inner"><h2>Heather Bergstein</h2><h3>Director, Digital Strategy</h3></div></div>
+			</div>
+		</div>
+		<div class="services-section clearfix">
+			<img align="left" src="<?= base_url(); ?>img/heather.jpg" />
+			<div class="project-label">
+				<div class="project-label-inner">
+					<h2>Heather Bergstein</h2>
+					<h3>Director, Strategy & Analytics</h3>
+					<p>A seasoned veteran in digital marketing on behalf of global brands, Heather joins Raison D’Être as Director of Strategy & Analytics. Prior to joining, Heather held the executive position of Channel Communications Manager for Skincare at Unilever following her previous role as Director of Corporate Digital Marketing & Media at The Estee Lauder Companies. During her time at The Estee Lauder Companies, she served as a company thought leader, driving overall digital media strategy and best practices globally while elevating the company’s digital media activities across a portfolio of 29 luxury beauty and fragrance brands. Additional experience includes positions at esteemed brands L’Oreal and Ralph Lauren.</p>
+					<p>An expert at partnering across brands, agencies, and publishers, Heather leveraged her 13 years of experience in beauty and fashion to advise the Estee Lauder brands as they crafted digital media strategies that ultimately drove impact across their paid, owned and earned brand platforms.</p>
+					<p>Heather currently serves on the IAB Digital Video Committee and was named one of the  “Brand innovators 40 Under 40, Class of 2013”.</p>
+					<p>Heather received her MBA from Fordham University and her undergraduate degree from NYU.</p>
 				</div>
-			</li>
-		</ul>
+			</div>
+		</div>
 	</div>
 </div>
